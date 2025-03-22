02bb632ff7c1082a459dcecf2cd771b619a4d850
JC <juan.grisalesarias@gmail.com>
2025-03-22 14:16:42 -0400

    Reto 1 resuelto

---
+++ utilityHost.sh
@@ -0,0 +1,11 @@
+# !/bin/sh
+# Description: This script will ask for your name and age and then print it out.
+# Author: JC - @jc
+# Date: 2025-22-03
+
+
+echo "What is your name?"
+read name
+echo "What is your age?"
+read age
+echo "You are $name and you are $age years old"
