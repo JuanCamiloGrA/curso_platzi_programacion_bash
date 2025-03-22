d73fbebea912eba28a9582f4fcb445e6b8d3d4f2
JC <juan.grisalesarias@gmail.com>
2025-03-22 18:24:51 -0400

    Descarga de archivos - wget

---
+++ 10_download.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Programa para ejemplificar el uso de la descarga de información desde internet utilizando wget
+# Autor: JC
+# Fecha 2025-22-03
+
+echo "Descargar información de internet"
+wget https://sample-files.com/downloads/documents/txt/simple.txt
