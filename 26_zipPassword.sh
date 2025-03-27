4c41add62a17e46276cd7faaee91f05f9d3b8039
jc <jc@humans.com>
2025-03-27 10:59:45 -0500

    Zip con contraseña

---
+++ 26_zipPassword.sh
@@ -0,0 +1,7 @@
+# !/bin/sh
+# Description: Programa para ejemplificar el empaquetamiento con clave utilizando zip
+# Author: JC - @jc
+# Date: 2025-03-26
+
+echo "Empaquetar todos los scripts de la carpeta actual en un archivo zip y asignar una clave"
+zip -e shellCourse.zip *.sh
