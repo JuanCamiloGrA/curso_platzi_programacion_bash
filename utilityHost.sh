f3bb15425ec4656105d56c8e78340871ca06af4b
jc <jc@humans.com>
2025-03-26 22:01:41 -0500

    Reto 5

--- utilityHost.sh
+++ utilityHost.sh
@@ -1,11 +1,20 @@
 # !/bin/sh
-# Description: This script will ask for your name and age and then print it out.
+# Description: This script will ask for the user's name and age, and then create a log file with the current date and time. Platzi Challenge 5.
 # Author: JC - @jc
-# Date: 2025-22-03
+# Date: 2025-03-26
 
 
 echo "What is your name?"
 read name
 echo "What is your age?"
 read age
-echo "You are $name and you are $age years old"
+
+echo -e "\nCreating log file..."
+
+# Create a log file with the current date
+filename=$(date +%Y-%m-%d_%H-%M-%S.log)
+echo "$(date) - User $name, aged $age years old, logged in." >> $filename
+
+echo "Log file: $filename"
+
+echo -e "\nLog file created successfully! Please check the log file for details."
